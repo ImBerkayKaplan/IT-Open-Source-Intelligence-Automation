c90f7bf9430736067f9a4151484ebf97a462ec2e
Kaplan <kaplan.298@osu.edu>
2019-01-17 15:13:52 -0500

    Added the GUI and the getRobtex method

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,93 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class Main {
+	
+	private static JFrame GUI(String title, Dimension size, boolean resizable){
+		
+		// Define the frame
+		JFrame frame;
+		
+		// Set the general proporties of jframe
+		frame = new JFrame();
+		frame.setSize(size);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle(title);
+		frame.setResizable(resizable);
+		return frame;
+	}
 	
 	public static void main(String[] args) {
 		
 		// Initialize the necessary tools
 		OSUIntelligenceTools OSU = new OSUIntelligenceTools();
+		JFrame frame = GUI("Open Source Intelligence", new Dimension(500,400), false);
 		
-		// Get the company name as index 1 and website as index 0
-		String info[] = OSU.getInfo();
+		// Define components
+		JPanel panel = new JPanel();
+		JTextField compNameT = new JTextField(), compWebsiteT = new JTextField();
+		JTextArea screen = new JTextArea();
+		JLabel compNameL = new JLabel("Enter company name: "),compWebsiteL = new JLabel("Enter company website: ");
+		JButton submit = new JButton("Submit");
+		JScrollPane fullScreen = new JScrollPane(screen);
 		
-		// Just a line seperator
-		System.out.println();
+		// Adjust properties of components
+		panel.setBackground(Color.GRAY);
+		fullScreen.setPreferredSize(new Dimension(450,250));
+		compNameT.setPreferredSize(new Dimension(300,25));
+		compWebsiteT.setPreferredSize(new Dimension(300,25));
+		submit.setSize(new Dimension(80,20));
+		submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	
+            	// Get the input variables and clear the screen
+            	String name = compNameT.getText(), website = compWebsiteT.getText();
+            	screen.setText("");
+            	
+            	// Edgar step
+            	screen.setText(screen.getText() + "Started searching on Edgar www.sec.gov\n");
+            	screen.setText(screen.getText() + OSU.getEdgar(name) + "\n\n");
+            	
+            	// Privacy Rights step
+            	screen.setText(screen.getText() + "Started searching on privacyrights.org.\n");
+            	screen.setText(screen.getText() + OSU.getPrivacyRights(name) + "\n\n");
+            	
+            	// Robtex step
+            	screen.setText(screen.getText() + "Started searching on www.robtex.com.\n");
+            	String robtext[] = OSU.getRobtex(website);
+            	
+            	for(int i = 0; i < robtext.length; i++) {
+            		screen.setText(screen.getText() + robtext[i] + "\n");
+            	}
+            	
+            	// Clear the inputs
+            	compNameT.setText("");
+            	compWebsiteT.setText("");
+            }
+        });
 		
-		// Hoovers step
-		System.out.println("Started searching on www.hoovers.com");
-		System.out.println(OSU.getHoovers(info[1]) + "\n");
+		// Add the components
+		frame.getContentPane().add(panel);
+		panel.add(compNameL);
+		panel.add(compNameT);
+		panel.add(compWebsiteL);
+		panel.add(compWebsiteT);
+		panel.add(submit);
+		panel.add(fullScreen);
 		
-		// Edgar step
-		System.out.println("Started searching on Edgar www.sec.gov");
-		System.out.println(OSU.getEdgar(info[1]) + "\n");
-		
-		// Privacy Rights step
-		System.out.println("Started searching on privacyrights.org.");
-		System.out.println(OSU.getPrivacyRights(info[1]) + "\n");
-		
-		// Whois step
-		System.out.println("Started searching on whois.domaintools.com. If step fails, make sure you did not reach your lookup limit in whois.domaintools.com.");
-		System.out.println(OSU.getWhoIs(info[0])[0] + "\n");
+		// Finish the frame
+		frame.setVisible(true);
 		
 	}
-
 }
--- src/OSUIntelligenceTools.java
+++ src/OSUIntelligenceTools.java
@@ -1,40 +1,16 @@
 import java.util.Iterator;
-import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class OSUIntelligenceTools {
 	
 	public static final String ERROR_MESSAGE = "Step failed due to a technical error. Contact the programmer please.";
 	public static final String NO_INFORMATION_MESSAGE = "No information found. Please proceed";
-	public static final int NUMBER_OF_WHOISELEMENTS = 4;
+	public static final int NUMBER_OF_ROBTEXELEMENTS = 8;
 	
-	/*
-	 * TODO: complete method header
-	 */
-	String[] getInfo() {
-		
-		// Initialize the result and the Scanner to get input and return
-		String result[] = new String[2];
-		Scanner in = new Scanner(System.in);
-		
-		// Get the result from the user and close the scanner
-		System.out.println("Enter the company website please");
-		result[0] = in.nextLine();
-		System.out.println("Enter the company name please");
-		result[1] = in.nextLine();
-		in.close();
-		
-		// Return the company website as the first element and the company name as the second element
-		return result;
-	}
-	
-	/*
-	 * TODO: Complete method header
-	 */
 	String getHoovers(String name) {
 		
 		// Initalize the result
@@ -43,7 +19,7 @@ public class OSUIntelligenceTools {
 		try {
 			
 			// Get the page
-			String website = "http://www.hoovers.com/company-information/company-search.html?term=" + name;
+			String website = "http://www.hoovers.com/company-information/company-search.html?term=" + name + "&aka_re=1";
 			Document doc = Jsoup.connect(website).get();
 					
 			// Check if any information exists by seeing if the specific class exists
@@ -54,9 +30,6 @@ public class OSUIntelligenceTools {
 			}
 					
 		}catch(Exception e) {
-			
-			// Print error message
-			//System.out.println(e);
 			result = ERROR_MESSAGE;
 		}
 		
@@ -66,11 +39,10 @@ public class OSUIntelligenceTools {
 	
 	/* This method checks if any information about the company exist on Edgar and returns the appropriate message.
 	 * 
-	 * @requires name
+	 * @params name
 	 * 		The company name in question
 	 * 
-	 * @returns result
-	 * 		The message that indicates if anything exist on Edgar owned by www.sec.gov
+	 * @returns The message that indicates if anything exist on Edgar owned by www.sec.gov
 	 */
 	String getEdgar(String name) {
 		
@@ -92,9 +64,6 @@ public class OSUIntelligenceTools {
 			}
 					
 		}catch(Exception e) {
-			
-			// Print error message
-			//System.out.println(e);
 			result = ERROR_MESSAGE;
 		}
 				
@@ -104,11 +73,10 @@ public class OSUIntelligenceTools {
 	
 	/* This method checks if the company has any breaches on www.privacyrights.org and returns the appropriate message.
 	 * 
-	 * @requires name
+	 * @params name
 	 * 		The company name in question
 	 * 
-	 * @returns result
-	 * 		The message that indicates if anything exist on PrivacyRights
+	 * @returns The message that indicates if anything exist on PrivacyRights
 	 */
 	String getPrivacyRights(String name) {
 		
@@ -132,9 +100,6 @@ public class OSUIntelligenceTools {
 			}
 			
 		}catch(Exception e) {
-			
-			// Print error message
-			//System.out.println(e);
 			result = ERROR_MESSAGE;
 		}
 		
@@ -144,41 +109,47 @@ public class OSUIntelligenceTools {
 	
 	/* This method checks the information on http://whois.domaintools.com/
 	 * 
-	 * @requires websiteName
+	 * @params websiteName
 	 * 		The website name in question
 	 * 
-	 * @returns result
-	 * 		The information found on whois as String array
+	 * @returns The information found on whois as String array
 	 */
-	String[] getWhoIs(String websiteName) {
+	String[] getRobtex(String websiteName) {
 		
 		// Initalize the result
-		String[] result = new String[NUMBER_OF_WHOISELEMENTS];
+		String[] result = new String[NUMBER_OF_ROBTEXELEMENTS];
 				
 		try {
 					
 			// Get the page
-			String website = "http://whois.domaintools.com/" + websiteName;
+			String website = "https://www.robtex.com/dns-lookup/" + websiteName;
 			Document doc = Jsoup.connect(website).get();
 			
-			// Get all the necessary rows in the page
-			Elements elements= doc.getElementsByClass("row-label");
+			// Get all the necessary rows in the page and convert to nodes
+			Elements elements = doc.getElementsByClass("table2col");
+			Iterator<Node> it = elements.first().child(1).childNodes().iterator();
+			int i = 0;
 			
-			// Set up the iterator and insert the worst case messages
-			Iterator<Element> it = elements.iterator();
-			result[0] = "IP address could not be found";
-			result[1] = "Server Location unkown";
-			result[2] = "Server host if third party unkown";
-			result[3] = "Server type unkown";
-			
+			// Go through all element
 			while(it.hasNext()) {
-				System.out.println(it.next());
-			};
+				Node node = it.next();
+				
+				// Make sure only the columns with data are taken into account
+				if(node.childNodeSize() > 1) {
+					
+					// Put the header to the result. It will be followed by the data. Get the child of the second element which contains the data
+					result[i] = node.childNode(0).childNode(0).toString() + ": ";
+					Node child = node.childNode(1);
+					
+					// Get all data of the tree
+					result[i] = result[i] + getJsoupTreeData(child);
+					
+					// Increment the counter to place the data correctly in result
+					i++;
+				}
+			}
 
 		}catch(Exception e) {
-			
-			// Print error message
-			//System.out.println(e);
 			
 			// Set all elements the error message
 			for(int i = 0; i < result.length; i++) {
@@ -189,6 +160,36 @@ public class OSUIntelligenceTools {
 		// Return the Privacy Rights breaches
 		return result;
 		
+	}
+	
+	/* A private internal method that gets all concatenated leaves of the Jsoup tree with a little of customization for the robtex website
+	 * 
+	 * @params root
+	 * 		The tree in Jsoup
+	 * 
+	 * @returns all concatenated leaves of root
+	 */
+	private String getJsoupTreeData(Node root) {
+	
+		String result = "";
+		
+		// Go through all child nodes
+		for(int i = 0; i < root.childNodeSize(); i++) {
+			
+			// If the root still has children, go to the leftmost node, otherwise, get get the data
+			if(root.childNode(i).childNodeSize()>0) {
+				result = result + getJsoupTreeData(root.childNode(i))+" ";
+			}else {
+				
+				// A custom solution to prevent unwanted <ul>s appearing at the end of the string
+				if(!root.childNode(i).nodeName().equals("ul")) {
+					result = result + root.childNode(i).toString();
+				}
+			}
+		}
+		
+		// Return the leaf
+		return result;
 	}
 	
 }
